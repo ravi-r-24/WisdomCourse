06402b248313423afc913ca8b7a5510f7e6a3565
k-ravi24 <k.ravi19777@gmail.com>
2024-08-29 10:30:19 +0530

    created: apiError && apiResponse utils file

---
+++ src/utils/apiError.js
@@ -0,0 +1,10 @@
+class apiError extends Error {
+  constructor(statusCode, message) {
+    super(message);
+    this.statusCode = statusCode;
+    this.message = message;
+    this.success = false;
+  }
+}
+
+export default apiError;
