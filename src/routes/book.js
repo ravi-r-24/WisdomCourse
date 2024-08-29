e824c1c7ed8807e18f47e9db958c75ad8586e4c7
k-ravi24 <k.ravi19777@gmail.com>
2024-08-29 10:29:52 +0530

    added: middleware in bookRegister to upload file

--- src/routes/book.js
+++ src/routes/book.js
@@ -1,8 +1,18 @@
 import { Router } from "express";
+import { register } from "../controller/book.js";
+import upload from "../middleware/multer.js";
 
 const router = new Router();
 
 // routes
-router.route("/register", registerController);
+router.route("/register").post(
+  upload.fields([
+    {
+      name: "cover_photo",
+      maxCount: 1,
+    },
+  ]),
+  register
+);
 
 export default router;
