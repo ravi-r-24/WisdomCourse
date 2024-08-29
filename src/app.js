cc936f1bd90a206e78ca78c0b6a362365fa06bfb
k-ravi24 <k.ravi19777@gmail.com>
2024-08-29 10:27:48 +0530

    added: middleware for url data receive && static folder

--- src/app.js
+++ src/app.js
@@ -3,5 +3,16 @@ import bookRouter from "./routes/book.js";
 
 export const app = express();
 
+// middleware to receive data from url
+app.use(
+  express.urlencoded({
+    extended: true,
+    limit: "10000",
+  })
+);
+
+// middleware to receive public file data
+app.use(express.static("./uploads"));
+
 // book routes
 app.use("/api/v1/book", bookRouter);
