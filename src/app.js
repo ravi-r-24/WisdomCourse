14f3bfa93109a4e23687903e5d0283c69583ecd0
k-ravi24 <k.ravi19777@gmail.com>
2024-08-29 14:47:15 +0530

    updated: series router

--- src/app.js
+++ src/app.js
@@ -4,18 +4,17 @@ import seriesRouter from "./routes/series.js";
 
 export const app = express();
 
-// middleware to receive data from url
+// Middleware to parse URL-encoded bodies
+app.use(express.urlencoded({ extended: true }));
+// middleware to allow receiving json data file
 app.use(
-  express.urlencoded({
-    extended: true,
+  express.json({
     limit: "10000",
   })
 );
-
 // middleware to receive public file data
 app.use(express.static("./uploads"));
-
 // book routes
 app.use("/api/v1/book", bookRouter);
 // series routes
-app.use("api/v1/series", seriesRouter);
+app.use("/api/v1/series", seriesRouter);
