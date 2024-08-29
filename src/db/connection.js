b0c7c49b9558c6d70463983b380e16e770295dc7
k-ravi24 <k.ravi19777@gmail.com>
2024-08-29 10:28:29 +0530

    uri/db_name --> uridbname

--- src/db/connection.js
+++ src/db/connection.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 export const databaseConnection = async () => {
   try {
     const connectionInstance = await mongoose.connect(
-      `${process.env.MONGODB_URI}/${process.env.DB_NAME}`
+      `${process.env.MONGODB_URI}${process.env.DB_NAME}`
     );
 
     console.log(`Connecting host: ${connectionInstance.connection.host}`);
